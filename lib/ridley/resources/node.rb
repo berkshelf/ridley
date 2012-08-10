c1ebba077e7fd9ce0a6525a6358d6a0cd9da61d4
Jamie Winsor <jamie@vialstudios.com>
2012-08-10 13:27:25 -0700

    define node resource

--- lib/ridley/resources/node.rb
+++ lib/ridley/resources/node.rb
@@ -7,5 +7,28 @@ module Ridley
     end
 
     include Ridley::Resource
+
+    set_chef_id "name"
+    set_chef_type "node"
+    set_chef_json_class "Chef::Node"
+    set_resource_path "nodes"
+
+    attribute :name
+    validates_presence_of :name
+
+    attribute :description, default: String.new
+    validates_presence_of :description
+
+    attribute :attributes, default: Hash.new
+    validates_presence_of :attributes
+
+    attribute :overrides, default: Hash.new
+    validates_presence_of :overrides
+
+    attribute :defaults, default: Hash.new
+    validates_presence_of :defaults
+
+    attribute :run_list, default: Array.new
+    validates_presence_of :run_list
   end
 end
