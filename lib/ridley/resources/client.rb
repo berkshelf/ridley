a05f607bd59f07d49344a4cbc4d0ef849b011c69
Jamie Winsor <jamie@vialstudios.com>
2012-08-15 19:36:08 -0700

    add ability to regenerate private keys to Client

--- lib/ridley/resources/client.rb
+++ lib/ridley/resources/client.rb
@@ -8,6 +8,25 @@ module Ridley
 
     include Ridley::Resource
 
+    class << self
+      # Retrieves a client from the remote connection matching the given chef_id
+      # and regenerates it's private key. An instance of the updated object will
+      # be returned and have a value set for the 'private_key' accessor.
+      #
+      # @param [String, #chef_id] client
+      #
+      # @raise [Errors::HTTPNotFound]
+      #   if a client with the given chef_id is not found
+      # @raise [Errors::HTTPError]
+      #
+      # @return [Ridley::Client]
+      def regenerate_key(client)
+        obj = find!(client)
+        obj.regenerate_key
+        obj
+      end
+    end
+
     set_chef_id "name"
     set_chef_type "client"
     set_chef_json_class "Chef::ApiClient"
@@ -17,9 +36,29 @@ module Ridley
     validates_presence_of :name
 
     attribute :admin, default: false
-    validates_presence_of :admin
+    validates_inclusion_of :admin, in: [ true, false ]
 
+    attribute :validator, default: false
+    validates_inclusion_of :validator, in: [ true, false ]
+
+    attribute :certificate
     attribute :public_key
     attribute :private_key
+    attribute :orgname
+
+    def attributes
+      super.except(:json_class)
+    end
+
+    # Regenerates the private key of the instantiated client object. The new
+    # private key will be set to the value of the 'private_key' accessor
+    # of the instantiated client object.
+    #
+    # @return [Boolean]
+    #   true for success and false for failure
+    def regenerate_key
+      self.private_key = true
+      self.save
+    end
   end
 end
--- spec/unit/ridley/resources/client_spec.rb
+++ spec/unit/ridley/resources/client_spec.rb
@@ -2,4 +2,50 @@ require 'spec_helper'
 
 describe Ridley::Client do
   it_behaves_like "a Ridley Resource", Ridley::Client
+
+  let(:connection) { double('connection') }
+
+  before(:each) do
+    Ridley::Connection.active = connection
+  end
+
+  describe "ClassMethods" do
+    subject { Ridley::Client }
+
+    describe "::regenerate_key" do
+      let(:client) { double('client', name: "ridley-test") }
+
+      it "finds the given client and regenerates it's key" do
+        client.should_receive(:regenerate_key)
+        subject.should_receive(:find!).with("ridley-test").and_return(client)
+        
+        subject.regenerate_key("ridley-test")
+      end
+
+      it "returns the updated client" do
+        client.should_receive(:regenerate_key)
+        subject.should_receive(:find!).with("ridley-test").and_return(client)
+
+        subject.regenerate_key("ridley-test").should eql(client)
+      end
+    end
+  end
+
+  subject do
+    Ridley::Client.new(name: "ridley-test", admin: false)
+  end
+
+  describe "#regenerate_key" do
+    it "returns true if successful" do
+      subject.should_receive(:save).and_return(true)
+
+      subject.regenerate_key.should be_true
+    end
+
+    it "returns false if not successful" do
+      subject.should_receive(:save).and_return(false)
+
+      subject.regenerate_key.should be_false
+    end
+  end
 end
