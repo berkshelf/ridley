53eb2c6803f12f7e2328f8a85528016a588b50a4
Jamie Winsor <jamie@vialstudios.com>
2012-10-08 17:13:55 -0700

    add ability to easily set environment attributes

--- lib/ridley/resources/node.rb
+++ lib/ridley/resources/node.rb
@@ -18,32 +18,75 @@ module Ridley
     attribute :override, default: HashWithIndifferentAccess.new
     attribute :run_list, default: HashWithIndifferentAccess.new
 
+    # @param [Hash] hash
     def automatic=(hash)
       super(HashWithIndifferentAccess.new(hash))
     end
 
+    # @param [Hash] hash
     def normal=(hash)
       super(HashWithIndifferentAccess.new(hash))
     end
 
+    # @param [Hash] hash
     def default=(hash)
       super(HashWithIndifferentAccess.new(hash))
     end
 
+    # @param [Hash] hash
     def override=(hash)
       super(HashWithIndifferentAccess.new(hash))
     end
 
+    # Set a node level override attribute given the dotted path representation of the Chef
+    # attribute and value
+    # 
+    # @example setting and saving a node level override attribute
+    #
+    #   obj = node.find("jwinsor-1")
+    #   obj.set_override_attribute("my_app.billing.enabled", false)
+    #   obj.save
+    #
+    # @param [String] key
+    # @param [Object] value
+    #
+    # @return [HashWithIndifferentAccess]
     def set_override_attribute(key, value)
       attr_hash = HashWithIndifferentAccess.from_dotted_path(key, value)
       self.override = self.override.merge(attr_hash)
     end
 
+    # Set a node level default attribute given the dotted path representation of the Chef
+    # attribute and value
+    # 
+    # @example setting and saving a node level default attribute
+    #
+    #   obj = node.find("jwinsor-1")
+    #   obj.set_default_attribute("my_app.billing.enabled", false)
+    #   obj.save
+    #
+    # @param [String] key
+    # @param [Object] value
+    #
+    # @return [HashWithIndifferentAccess]
     def set_default_attribute(key, value)
       attr_hash = HashWithIndifferentAccess.from_dotted_path(key, value)
       self.default = self.default.merge(attr_hash)
     end
 
+    # Set a node level normal attribute given the dotted path representation of the Chef
+    # attribute and value
+    # 
+    # @example setting and saving a node level normal attribute
+    #
+    #   obj = node.find("jwinsor-1")
+    #   obj.set_normal_attribute("my_app.billing.enabled", false)
+    #   obj.save
+    #
+    # @param [String] key
+    # @param [Object] value
+    #
+    # @return [HashWithIndifferentAccess]
     def set_normal_attribute(key, value)
       attr_hash = HashWithIndifferentAccess.from_dotted_path(key, value)
       self.normal = self.normal.merge(attr_hash)
--- spec/unit/ridley/resources/environment_spec.rb
+++ spec/unit/ridley/resources/environment_spec.rb
@@ -70,4 +70,68 @@ describe Ridley::Environment do
       end
     end
   end
+
+  let(:connection) { double('connection') }
+
+  subject { Ridley::Environment.new(connection) }
+
+  describe "#set_override_attribute" do
+    it "returns a HashWithIndifferentAccess" do
+      subject.set_override_attribute('deep.nested.item', true).should be_a(HashWithIndifferentAccess)
+    end
+
+    it "sets an override node attribute at the nested path" do
+       subject.set_override_attribute('deep.nested.item', true)
+
+       subject.override_attributes.should have_key("deep")
+       subject.override_attributes["deep"].should have_key("nested")
+       subject.override_attributes["deep"]["nested"].should have_key("item")
+       subject.override_attributes["deep"]["nested"]["item"].should be_true
+    end
+
+    context "when the override attribute is already set" do
+      it "test" do
+        subject.override_attributes = {
+          deep: {
+            nested: {
+              item: false
+            }
+          }
+        }
+        subject.set_override_attribute('deep.nested.item', true)
+        
+        subject.override_attributes["deep"]["nested"]["item"].should be_true
+      end
+    end
+  end
+
+  describe "#set_default_attribute" do
+    it "returns a HashWithIndifferentAccess" do
+      subject.set_default_attribute('deep.nested.item', true).should be_a(HashWithIndifferentAccess)
+    end
+
+    it "sets an override node attribute at the nested path" do
+       subject.set_default_attribute('deep.nested.item', true)
+
+       subject.default_attributes.should have_key("deep")
+       subject.default_attributes["deep"].should have_key("nested")
+       subject.default_attributes["deep"]["nested"].should have_key("item")
+       subject.default_attributes["deep"]["nested"]["item"].should be_true
+    end
+
+    context "when the override attribute is already set" do
+      it "test" do
+        subject.default_attributes = {
+          deep: {
+            nested: {
+              item: false
+            }
+          }
+        }
+        subject.set_default_attribute('deep.nested.item', true)
+        
+        subject.default_attributes["deep"]["nested"]["item"].should be_true
+      end
+    end
+  end
 end
