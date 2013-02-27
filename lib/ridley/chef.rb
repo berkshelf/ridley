92d56fc7c98e91de55a765c6835ea4885812deee
Jamie Winsor <jamie@vialstudios.com>
2013-02-26 20:29:02 -0800

    fix file header

--- lib/ridley/chef.rb
+++ lib/ridley/chef.rb
@@ -1,5 +1,5 @@
 module Ridley
-  # @author Jamie Winsor <jamie@vialstudios.com.com>
+  # @author Jamie Winsor <jamie@vialstudios.com>
   #
   # Classes and modules used for integrating with a Chef Server, the Chef community
   # site, and Chef Cookbooks
