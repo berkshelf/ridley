a9eaa507e2460cd5cf8352dffcca4c4ad85283f5
Kyle Allan <kallan@riotgames.com>
2013-04-05 13:40:31 -0700

    add spec for #chef_client

--- spec/unit/ridley/host_connector/ssh_spec.rb
+++ spec/unit/ridley/host_connector/ssh_spec.rb
@@ -38,11 +38,36 @@ describe Ridley::HostConnector::SSH do
     end
   end
 
-  subject { Ridley::HostConnector::SSH.new([node_one, node_two], user: "vagrant", password: "vagrant") }
+  subject { Ridley::HostConnector::SSH.new([node_one, node_two], ssh: {user: "vagrant", password: "vagrant"}) }
 
   describe "#run" do
-    it "returns an SSH::ResponseSet" do
+    it "returns an HostConnector::ResponseSet" do
       subject.run("ls").should be_a(Ridley::HostConnector::ResponseSet)
+    end
+  end
+
+  describe "#chef_client" do
+    it "returns a HostConnector::ResponseSet" do
+      subject.chef_client.should be_a(Ridley::HostConnector::ResponseSet)
+    end
+
+    context "when the sudo key exists" do
+      let(:options) do
+        {
+          ssh: {
+            user: "vagrant",
+            password: "vagrant",
+            sudo: true
+          }
+        }
+      end
+
+      subject { Ridley::HostConnector::SSH.new(node_one, options) }
+
+      it "receives a run command with 'sudo'" do
+        subject.should_receive(:run).with("sudo chef-client")
+        subject.chef_client
+      end
     end
   end
 end
