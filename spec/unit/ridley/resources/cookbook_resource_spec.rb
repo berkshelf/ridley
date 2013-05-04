21015117a6fa4e71a63508b12d20710806726400
Jamie Winsor <jamie@vialstudios.com>
2013-05-04 15:01:31 -0700

    fix downloading/uploading of cookbooks

--- spec/unit/ridley/resources/cookbook_resource_spec.rb
+++ spec/unit/ridley/resources/cookbook_resource_spec.rb
@@ -3,35 +3,49 @@ require 'spec_helper'
 describe Ridley::CookbookResource do
   let(:client_name) { "reset" }
   let(:client_key) { fixtures_path.join('reset.pem') }
-  let(:connection) { double('connection') }
+  let(:connection) { Ridley::Connection.new("http://localhost:8889", "reset", fixtures_path.join("reset.pem").to_s) }
   subject { described_class.new(double('registry'), client_name, client_key) }
   before  { subject.stub(connection: connection) }
 
   describe "#download" do
-    pending
+    let(:name) { "example_cookbook" }
+    let(:version) { "0.1.0" }
+    let(:destination) { tmp_path.join("example_cookbook-0.1.0").to_s }
+
+    context "when the cookbook of the name/version is found" do
+      it "downloads the cookbook to the destination" do
+        pending "can't test downloading until https://github.com/jkeiser/chef-zero/issues/5 is fixed"
+      end
+    end
+
+    context "when the cookbook of the name/version is not found" do
+      before { subject.should_receive(:find).with(name, version).and_return(nil) }
+
+      it "raises a ResourceNotFound error" do
+        expect {
+          subject.download(name, version, destination)
+        }.to raise_error(Ridley::Errors::ResourceNotFound)
+      end
+    end
   end
 
   describe "#latest_version" do
     let(:name) { "ant" }
 
-    before(:each) do
-      subject.should_receive(:versions).with(name).and_return(versions)
-    end
-
     context "when the cookbook has no versions" do
-      let(:versions) { Array.new }
-
       it "returns nil" do
         subject.latest_version(name).should be_nil
       end
     end
 
     context "when the cookbook has versions" do
-      let(:versions) do
-        [ "1.0.0", "1.2.0", "3.0.0", "1.4.1" ]
+      before do
+        chef_cookbook(name, "1.0.0")
+        chef_cookbook(name, "1.2.0")
+        chef_cookbook(name, "3.0.0")
       end
 
-      it "returns nil" do
+      it "returns the latest version" do
         subject.latest_version(name).should eql("3.0.0")
       end
     end
@@ -81,7 +95,36 @@ describe Ridley::CookbookResource do
   end
 
   describe "#satisfy" do
-    pending
+    let(:name) { "ridley_test" }
+
+    before { chef_cookbook(name, "1.0.0") }
+
+    context "when there is a solution" do
+      before do
+        chef_cookbook(name, "2.0.0")
+        chef_cookbook(name, "3.0.0")
+      end
+
+      it "returns a CookbookObject" do
+        subject.satisfy(name, ">= 2.0.0").should be_a(Ridley::CookbookObject)
+      end
+
+      it "is the best solution" do
+        subject.satisfy(name, ">= 2.0.0").version.should eql("3.0.0")
+      end
+    end
+
+    context "when there is no solution" do
+      it "returns nil" do
+        subject.satisfy(name, ">= 2.0.0").should be_nil
+      end
+    end
+
+    context "when the cookbook does not exist" do
+      it "returns nil" do
+        subject.satisfy(name, ">= 1.2.3").should be_nil
+      end
+    end
   end
 
   describe "#upload" do
