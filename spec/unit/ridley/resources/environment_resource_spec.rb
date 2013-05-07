75fd55c84a29ee0f75d0f29ad1cc94c6ac0474ec
Jamie Winsor <jamie@vialstudios.com>
2013-05-06 18:09:07 -0700

    implement /environments/NAME/cookbook_versions
    as EnvironmentResource#cookbook_versions

--- spec/unit/ridley/resources/environment_resource_spec.rb
+++ spec/unit/ridley/resources/environment_resource_spec.rb
@@ -1,7 +1,60 @@
 require 'spec_helper'
 
 describe Ridley::EnvironmentResource do
-  subject { described_class.new(double('registry')) }
+  let(:server_url)  { Ridley::RSpec::ChefServer.server_url }
+  let(:client_name) { "reset" }
+  let(:client_key)  { fixtures_path.join('reset.pem').to_s }
+  let(:connection)  { Ridley::Connection.new(server_url, client_name, client_key) }
+
+  let(:resource) do
+    resource = described_class.new(double('registry'))
+    resource.stub(connection: connection)
+    resource
+  end
+
+  subject { resource }
+
+  describe "#cookbook_versions" do
+    let(:name) { "rspec-test" }
+    let(:run_list) { ["hello", "there"] }
+
+    subject { resource.cookbook_versions(name, run_list) }
+
+    context "when the chef server has the given cookbooks" do
+      before do
+        chef_environment("rspec-test")
+        chef_cookbook("hello", "1.2.3")
+        chef_cookbook("there", "1.0.0")
+      end
+
+      it "returns a Hash" do
+        should be_a(Hash)
+      end
+
+      it "contains a key for each cookbook" do
+        subject.keys.should have(2).items
+        subject.should have_key("hello")
+        subject.should have_key("there")
+      end
+    end
+
+    context "when the chef server does not have the environment" do
+      before do
+        chef_cookbook("hello", "1.2.3")
+        chef_cookbook("there", "1.0.0")
+      end
+
+      it "raises a ResourceNotFound error" do
+        expect { subject }.to raise_error(Ridley::Errors::ResourceNotFound)
+      end
+    end
+
+    context "when the chef server does not have one or more of the cookbooks" do
+      it "raises a precondition failed error" do
+        expect { subject }.to raise_error(Ridley::Errors::HTTPPreconditionFailed)
+      end
+    end
+  end
 
   describe "#delete_all" do
     let(:default_env) { double(name: "_default") }
