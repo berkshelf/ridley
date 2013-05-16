5434bfc2532d6bd67667137f5f1be07e5db29976
Seth Vargo <sethvargo@gmail.com>
2013-05-15 23:44:21 -0400

    Use require_relative in chef_objects.rb

--- lib/ridley/chef_objects.rb
+++ lib/ridley/chef_objects.rb
@@ -1,3 +1,3 @@
 Dir["#{File.dirname(__FILE__)}/chef_objects/*.rb"].sort.each do |path|
-  require "ridley/chef_objects/#{File.basename(path, '.rb')}"
+  require_relative "chef_objects/#{File.basename(path, '.rb')}"
 end
