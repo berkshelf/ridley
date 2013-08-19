f6fc1deb13c3bceb45c3b2105599b8bcf88eb42e
Jeffrey Hulten <jhulten@whitepages.com>
2013-08-19 14:24:32 -0700

    Convert to expect rspec format

--- spec/unit/ridley/chef/cookbook/syntax_check_spec.rb
+++ spec/unit/ridley/chef/cookbook/syntax_check_spec.rb
@@ -17,10 +17,10 @@ describe Ridley::Chef::Cookbook::SyntaxCheck do
 
   describe "#ruby_files" do
     it "lists the rb files in a cookbook" do
-      subject.ruby_files.should include(cookbook_dir.join("libraries/my_lib.rb").to_s)
+      expect(subject.ruby_files).to include(cookbook_dir.join("libraries/my_lib.rb").to_s)
     end
     it "does not list the rb files in a cookbook that are ignored" do
-      subject.ruby_files.should_not include(cookbook_dir.join("ignores/magic.rb").to_s)
+      expect(subject.ruby_files).not_to include(cookbook_dir.join("ignores/magic.rb").to_s)
     end
   end
 
@@ -28,16 +28,16 @@ describe Ridley::Chef::Cookbook::SyntaxCheck do
     it "filters out validated rb files" do
       valid_ruby_file = cookbook_dir.join("libraries/my_lib.rb").to_s
       subject.validated(valid_ruby_file)
-      subject.untested_ruby_files.should_not include(valid_ruby_file)
+      expect(subject.untested_ruby_files).not_to include(valid_ruby_file)
     end
   end
 
   describe "#template_files" do
     it "lists the erb files in a cookbook" do
-      subject.template_files.should include(cookbook_dir.join("templates/default/temp.txt.erb").to_s)
+      expect(subject.template_files).to include(cookbook_dir.join("templates/default/temp.txt.erb").to_s)
     end
     it "does not list the erb files in a cookbook that are ignored" do
-      subject.template_files.should_not include(cookbook_dir.join("ignores/magic.erb").to_s)
+      expect(subject.template_files).not_to include(cookbook_dir.join("ignores/magic.erb").to_s)
     end
   end
 
@@ -45,7 +45,7 @@ describe Ridley::Chef::Cookbook::SyntaxCheck do
     it "filters out validated erb files" do
       valid_template_file = cookbook_dir.join("templates/default/temp.txt.erb").to_s
       subject.validated(valid_template_file)
-      subject.untested_template_files.should_not include(valid_template_file)
+      expect(subject.untested_template_files).not_to include(valid_template_file)
     end
   end
 
@@ -53,7 +53,7 @@ describe Ridley::Chef::Cookbook::SyntaxCheck do
     it "checks if a file has already been validated" do
       valid_template_file = cookbook_dir.join("templates/default/temp.txt.erb").to_s
       subject.validated(valid_template_file)
-      subject.validated?(valid_template_file).should be_true
+      expect(subject.validated?(valid_template_file)).to be_true
     end
   end
 
@@ -68,8 +68,8 @@ describe Ridley::Chef::Cookbook::SyntaxCheck do
       template_file = cookbook_dir.join("templates/default/temp.txt.erb").to_s
       file_checksum = Ridley::Chef::Digester.checksum_for_file(template_file)
 
-      validated_files.should_receive(:add).with(file_checksum)
-      subject.validated(template_file).should be_nil
+      expect(validated_files).to receive(:add).with(file_checksum)
+      expect(subject.validated(template_file)).to be_nil
     end
   end
 
@@ -77,17 +77,17 @@ describe Ridley::Chef::Cookbook::SyntaxCheck do
 
     it "asks #untested_ruby_files for a list of files and calls #validate_ruby_file on each" do
       subject.stub(:validate_ruby_file).with(anything()).exactly(9).times { true }
-      subject.validate_ruby_files.should be_true
+      expect(subject.validate_ruby_files).to be_true
     end
 
     it "marks the successfully validated ruby files" do
       subject.stub(:validated).with(anything()).exactly(9).times
-      subject.validate_ruby_files.should be_true
+      expect(subject.validate_ruby_files).to be_true
     end
 
     it "returns false if any ruby file fails to validate" do
       subject.stub(:validate_ruby_file).with(/\.rb$/) { false }
-      subject.validate_ruby_files.should be_false
+      expect(subject.validate_ruby_files).to be_false
     end
   end
 
@@ -95,17 +95,17 @@ describe Ridley::Chef::Cookbook::SyntaxCheck do
 
     it "asks #untested_template_files for a list of erb files and calls #validate_template on each" do
       subject.stub(:validate_template).with(anything()).exactly(9).times { true }
-      subject.validate_templates.should be_true
+      expect(subject.validate_templates).to be_true
     end
 
     it "marks the successfully validated erb files" do
       subject.stub(:validated).with(anything()).exactly(9).times
-      subject.validate_templates.should be_true
+      expect(subject.validate_templates).to be_true
     end
 
     it "returns false if any erb file fails to validate" do
       subject.stub(:validate_template).with(/\.erb$/) { false }
-      subject.validate_templates.should be_false
+      expect(subject.validate_templates).to be_false
     end
 
   end
@@ -120,6 +120,13 @@ describe Ridley::Chef::Cookbook::SyntaxCheck do
 
     it "asks #shell_out to check the files syntax"
 
+  end
+
+  describe "without a chefignore" do
+    let(:chefignore) { nil }
+    it "the file listing still works" do
+      expect(subject.ruby_files).to include(cookbook_dir.join("libraries/my_lib.rb").to_s)
+    end
   end
 
 end
