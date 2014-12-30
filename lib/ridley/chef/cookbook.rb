62924eca44615320bfe4de25821a892369e3fc13
MIZUNE Pine <pinemz@gmail.com>
2014-12-30 23:40:14 +0900

    Fix a encoding bug

--- lib/ridley/chef/cookbook.rb
+++ lib/ridley/chef/cookbook.rb
@@ -33,7 +33,7 @@ module Ridley::Chef
         path = Pathname.new(path)
 
         if (file = path.join(Metadata::COMPILED_FILE_NAME)).exist?
-          metadata = Metadata.from_json(File.read(file))
+          metadata = Metadata.from_json(File.read(file, :encoding => Encoding::UTF_8))
         elsif (file = path.join(Metadata::RAW_FILE_NAME)).exist?
           metadata = Metadata.from_file(file)
         else
